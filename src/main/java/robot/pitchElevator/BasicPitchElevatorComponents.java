ec21f0baba24a8523bf97238341a683f7d4c7cb0
DorRubinshtein <dorrub11@gmail.com>
2018-11-19 23:36:17 +0200

    made a ToggleByPID command file (its currently empty)

--- src/main/java/robot/pitchElevator/BasicPitchElevatorComponents.java
+++ src/main/java/robot/pitchElevator/BasicPitchElevatorComponents.java
@@ -2,6 +2,7 @@ package robot.pitchElevator;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.SpeedController;
 
 public class BasicPitchElevatorComponents implements PitchElevatorComponents {
@@ -24,6 +25,5 @@ public class BasicPitchElevatorComponents implements PitchElevatorComponents {
 
         MainSpeedController = new WPI_TalonSRX(28);
         //ClawSpeedController = new WPI_VictorSPX(29);
-
     }
 }
---
+++ src/main/java/robot/pitchElevator/ToggleByPID.java
@@ -0,0 +1,4 @@
+package robot.pitchElevator;
+
+public class ToggleByPID {
+}
